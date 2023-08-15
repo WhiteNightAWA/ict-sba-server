23598f5b39366fb787001fff90007d8e4200043c
WhiteNightAWA <evan.tang.2021@gmail.com>
2023-08-15 15:58:50 +0800

    Update

---
+++ models/shop.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+
+const shopSchema = new mongoose.Schema({
+    fn: String,
+    ln: String,
+    HKID: String,
+    vat: String,
+    shopName: String,
+    shopDesc: String,
+    avatar: String,
+    shopPhotos: [String],
+    phone: Number,
+    short: String,
+    rating: [{
+        username: String,
+        nick: Boolean,
+        rate: Number,
+        comment: String,
+        time: Date,
+    }],
+    position: [
+
+    ]
+});
+
+
+module.exports = mongoose.model("Shop", userSchema);
--- users/done.js
+++ users/done.js
@@ -1,10 +1,20 @@
 const User = require("../models/user");
+const Shop = require("../models/shop");
 const {hash} = require("bcrypt");
 
 
 const done = async (req, res) => {
     try {
         const { type } = req.body;
+
+        if ([type].includes(undefined)) {
+            return res.status(400).json({
+                error: "uncompleted_form",
+                error_description: "Somethings is undefined in { type }.",
+                code: 400,
+            });
+        }
+
         let user = await User.findOne({ user_id: req.user_id });
         if (type === "buy") {
             await User.findOneAndUpdate({ user_id: req.user_id }, {
@@ -16,15 +26,16 @@ const done = async (req, res) => {
                 msg: "Done Successfully!",
                 sell: false
             });
-        } else {
+        } else if (type === "sell") {
             const {fn, ln, HKID, vat, shopName, position} = req.body;
+            shop = await Shop.createOne({
+                fn, ln,
+                HKID: await hash(HKID, 12),
+                vat, shopName, position
+            })
             user = await User.findOneAndUpdate({ user_id: req.user_id }, {
                 type: "sell",
-                shopData: {
-                    fn, ln,
-                    HKID: await hash(HKID, 12),
-                    vat, shopName, position
-                }
+                shop: shop._id
             });
             return res.status(200).json({
                 code: 200,

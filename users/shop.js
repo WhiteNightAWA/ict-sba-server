a6e09248232086bc81f20985ac30d2a83a08a057
WhiteNightAWA <evan.tang.2021@gmail.com>
2023-08-15 16:13:00 +0800

    Update Shop

--- users/shop.js
+++ users/shop.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const Shop = require("../models/shop");
 const {hash} = require("bcrypt");
+const { ObjectId } = require("mongodb");
 
 
 const shop = async (req, res) => {
@@ -13,8 +14,20 @@ const shop = async (req, res) => {
                 code: 400,
             });
         }
-
         
+        shop = await Shop.findOne({ "_id": ObjectId(shopID) });
+
+        if (!shop) {
+            return res.status(400).json({
+                error: "invaild_shopId",
+                error_description: "Invaild Shop.",
+                code: 400,
+            });
+        } else {
+            shop.HKID = "-";
+            return res.status(200).json(shop);
+        }
+
 
     } catch (err) {
         console.log(err);
